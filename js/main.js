3843e2fde3a6fdb816c3bda49d266ccda265c906
Ilya <shumilov.iv@mail.ru>
2023-03-11 04:58:25 +0300

    fixed getRandomIntFromRange()

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 function getRandomIntFromRange(fromNum, toNum) {
+  if (typeof fromNum !== 'number' || typeof toNum !== 'number') {
+    console.error(new Error('Переданы значения, не являющиеся числами!'));
+  }
   if (fromNum > toNum) {
     console.error(new Error('Начальное значение диапазона больше конечного!'));
   }
   return Math.floor(Math.random() * (toNum + 1 - fromNum) + fromNum);
 }
-
-
