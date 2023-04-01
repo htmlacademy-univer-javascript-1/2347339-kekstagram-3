3136252a050272f8e12b4606071d96d6fbc8cb5f
Ilya <shumilov.iv@mail.ru>
2023-04-01 19:23:37 +0300

    fix listeners adding

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-import {generatePhotos} from './photos-loading.js';
+import { generatePhotos } from './photos-loading.js';
+import './upload-img-listeners.js';
 generatePhotos();
+
+
