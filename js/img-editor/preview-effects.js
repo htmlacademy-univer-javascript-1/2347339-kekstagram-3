a2e13489b091c62833a439d5164d9b3c38ac97d3
Ilya Shumilov <shumilov.iv@mail.ru>
2023-04-10 17:13:17 +0300

    some refactoring

--- js/img-editor/preview-effects.js
+++ js/img-editor/preview-effects.js
@@ -1,8 +1,10 @@
 import { getLastArrElem } from '../utils.js';
+import { changeSliderOptions } from './effect-slider.js';
+import { imgPreviewElem } from './img-editor.js';
 
 const DEFAULT_PREVIEW_EFFECT = 'effects__preview--none';
 const defaultPreviewEffectElem = document.querySelector('#effect-none');
-const imgPreviewElem = document.querySelector('.img-upload__preview');
+//const imgPreviewElem = document.querySelector('.img-upload__preview');
 const effectsListElem = document.querySelector('.effects__list');
 
 const onEffectItemClick = (evt) => {
@@ -11,6 +13,7 @@ const onEffectItemClick = (evt) => {
   if (targetClasses.contains('effects__preview')) {
     previewClasses.remove(getLastArrElem(previewClasses));
     previewClasses.add(getLastArrElem(targetClasses));
+    changeSliderOptions(evt.target.parentElement.previousElementSibling.value);
   }
 };
 
@@ -28,4 +31,4 @@ const resetPreviewEffects = () => {
   previewClasses.add(DEFAULT_PREVIEW_EFFECT);
   defaultPreviewEffectElem.checked  = true;
 };
-export {addEffectsListener, removeEffectsListener, resetPreviewEffects};
+export {addEffectsListener, removeEffectsListener, resetPreviewEffects, imgPreviewElem};
