49691f036c5fe15120a221f3f403eb73f56f5b0b
Ilya <shumilov.iv@mail.ru>
2023-04-12 02:05:13 +0300

    fixed hashtag show err

--- js/img-editor/text-validators.js
+++ js/img-editor/text-validators.js
@@ -1,5 +1,7 @@
-import { clearElemValue } from '../utils.js';
+import { clearElemValue, hasLegalLength } from '../utils.js';
 
+
+const HASHTAG_REG = new RegExp('(^$)|(^#[a-zA-Zа-яА-ЯёЁ0-9]{1,17}$)');
 const imgFormElem = document.querySelector('.img-upload__form');
 const hashtagElem = document.querySelector('.text__hashtags');
 const descriptionElem = document.querySelector('.text__description');
@@ -8,9 +10,15 @@ const pristine = new Pristine(imgFormElem, {
   errorClass: 'img-upload__form--invalid',
   successClass: 'img-upload__from--valid',
   errorTextParent: 'img-upload__text',
-  errorTextTag: 'span',
+  errorTextTag: 'div',
   errorTextClass: 'img-upload__form__error',
 });
+
+const isLegalHashtag = (str) => HASHTAG_REG.test(str);
+const isLegalDescription = (str) => !hasLegalLength(str, 19) && hasLegalLength(str, 140);
+
+pristine.addValidator(hashtagElem, isLegalHashtag);
+pristine.addValidator(descriptionElem, isLegalDescription);
 
 const resetTextValidators = () => {
   pristine.reset();
@@ -22,9 +30,15 @@ const clearTextInputs = () => {
 };
 
 imgFormElem.addEventListener('submit', (evt) => {
-  const isValid = pristine.validate();
-  if (!isValid) {
-    evt.preventDefault();
+  const isValidForm = pristine.validate();
+  if (isValidForm) {
+    return;
+  }
+  evt.preventDefault();
+  if (!isLegalDescription(descriptionElem.value)) {
+    pristine.addError(descriptionElem, 'Длина комментария должна быть от 20 до 140 символов');
+  } else if (!isLegalHashtag(hashtagElem.value)) {
+    pristine.addError(hashtagElem, 'Хештег должен начинаться с # и иметь от 1 до 17 букв и(или) цифр после');
   }
 });
 
