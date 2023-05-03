07dde6209753b4f4ca11d8c8c9ec57427b7b6046
Ilya <shumilov.iv@mail.ru>
2023-05-03 23:05:37 +0300

    added msg showing

--- js/img-editor/img-form-submit.js
+++ js/img-editor/img-form-submit.js
@@ -1,32 +1,48 @@
-import { createBtnBlocker, showAlert } from '../utils.js';
-//import { pristine } from './text-validators.js';
-//import { sendData } from '../api.js';
+import { getDescriptionInput, getHashtagInput, setHashtagInput, setDescriptionInput, pristine } from './text-validators.js';
+import { sendData } from '../api.js';
 import { closeImgEditor } from './img-editor.js';
 import { createMsgElem } from '../show-message.js';
+import { createBtnBlocker } from '../utils.js';
 
-const submitBtnElem = document.querySelector('.img-upload__submit');
-const submitBtnBlocker = createBtnBlocker(submitBtnElem, 'Отправляю...', 'Отправить');
+const submitBtnBlocker = createBtnBlocker('#upload-submit', 'Отправляю...', 'Отправить');
 const imgFormElem = document.querySelector('.img-upload__form');
-const successMsgElem = createMsgElem('#success', '.success', 'success__button', document.body);
 
 let imgFile;
 
 const onSubmitBtnClick = (evt) => {
   evt.preventDefault();
+  new Promise((resolve) => resolve(pristine.validate()))
+    .then((isValidForm) => {
+      if (!isValidForm) {
+        return;
+      }
+      submitBtnBlocker.block();
+      const body = new FormData(imgFormElem);
+      body.append('filename', imgFile);
+      sendData(showSuccess, showError, body);
+    })
+    .then(() => {
+    });
+};
+
+function showSuccess() {
+  submitBtnBlocker.unblock();
+  const successMsgElem = createMsgElem('#success', '.success', document.body, '.success__button', '.success__inner');
   closeImgEditor();
   successMsgElem.show();
-};
-  // new Promise((resolve) => resolve(pristine.validate()))
-  //   .then((isValidForm) => {
-  //     if (!isValidForm) {
-  //       return;
-  //     }
-  //     const body = new FormData(imgFormElem);
-  //     body.append(imgFile);
-  //     showSuccessMsg();
-//   //     // addSuccessMsgListeners();
-//     });
-// };
+}
+
+function showError() {
+  submitBtnBlocker.unblock();
+  const errorMsgElem = createMsgElem('#error', '.error', document.body, '.error__button', '.error__inner');
+  const hashtag = getHashtagInput();
+  const description = getDescriptionInput();
+  closeImgEditor();
+  errorMsgElem.show();
+  setHashtagInput(hashtag);
+  setDescriptionInput(description);       //effects, slider?
+}
+
 
 const addSubmitListener = () => {
   imgFormElem.addEventListener('submit', onSubmitBtnClick);
